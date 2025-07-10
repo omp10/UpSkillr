5bfa6ce3e6776616f31beaa31a5a1b7c600f035c
omp10 <omparteki2002@gmail.com>
2025-07-10 18:26:50 +0530

    Navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,19 @@
 
 import './App.css'
 import { Button } from './components/ui/button'
+import Navbar from './components/ui/Navbar'
 import Login from './pages/Login'
+import { ThemeProvider } from "./ThemeProvider";
 
 function App() {
 
   return (
-    <>
+    <main>
+      <ThemeProvider>
+      <Navbar/>
     <Login/>
-    </>
+    </ThemeProvider>
+    </main>
   )
 }
 
