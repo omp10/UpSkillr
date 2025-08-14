353e205a53da976d2c5131157fe859275d3e3f07
omp10 <omparteki2002@gmail.com>
2025-08-14 22:46:56 +0530

    test 3.8

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,24 @@
-import { createBrowserRouter } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import { Button } from "./components/ui/button";
+import { ThemeProvider } from "./ThemeProvider";
+import MainLayout from "./layout/MainLayout";
 import Navbar from "./components/ui/Navbar";
 import Login from "./pages/Login";
 import HeroSection from "./pages/student/HeroSection";
-import { ThemeProvider } from "./ThemeProvider";
-import MainLayout from "./layout/MainLayout";
-import { RouterProvider } from "react-router";
 import Courses from "./pages/student/Courses";
 import MyLearning from "./pages/student/MyLearning";
 import Profile from "./pages/student/Profile";
+import SearchPage from "./pages/student/SearchPage";
+import CourseDetail from "./pages/student/CourseDetail";
+import CourseProgress from "./pages/student/CourseProgress";
 import Sidebar from "./pages/admin/lecture/Sidebar";
 import Dashboard from "./pages/admin/Dashboard";
-import Course from "./pages/student/Course";
 import CourseTable from "./pages/admin/course/CourseTable";
 import AddCourse from "./pages/admin/course/AddCourse";
 import EditCourse from "./pages/admin/course/EditCourse";
 import CreateLecture from "./pages/admin/lecture/CreateLecture";
 import EditLecture from "./pages/admin/lecture/EditLecture";
-import CourseDetail from "./pages/student/CourseDetail";
-import CourseProgress from "./pages/student/CourseProgress";
-import SearchPage from "./pages/student/SearchPage";
+
 import {
   AdminRoute,
   AuthenticatedUser,
@@ -29,115 +27,100 @@ import {
 import PurchaseCourseProtectedRoute from "./components/PurchaseCourseProtectedRoute";
 
 function App() {
-  const appRouter = createBrowserRouter([
-    {
-      path: "/",
-      element: <MainLayout />,
-      children: [
-        {
-          path: "/",
-          element: (
-            <>
-              <HeroSection />
-              <Courses />
-            </>
-          ),
-        },
-        {
-          path: "login",
-          element: (
-            <AuthenticatedUser>
-              <Login />
-            </AuthenticatedUser>
-          ),
-        },
-        {
-          path: "my-learning",
-          element: (
-            <ProtectedRoute>
-              <MyLearning />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "profile",
-          element: (
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "course/search",
-          element: (
-            <ProtectedRoute>
-              <SearchPage />
-            </ProtectedRoute>
-          ),
-        },
-
-        {
-          path: "course-detail/:courseId",
-          element: (
-            <ProtectedRoute>
-              <CourseDetail />
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "course-progress/:courseId",
-          element: (
-            <ProtectedRoute>
-              <PurchaseCourseProtectedRoute>
-                <CourseProgress />
-              </PurchaseCourseProtectedRoute>
-            </ProtectedRoute>
-          ),
-        },
-        {
-          path: "admin",
-          element: (
-            <AdminRoute>
-              <Sidebar />
-            </AdminRoute>
-          ),
-          children: [
-            {
-              path: "dashboard",
-              element: <Dashboard />,
-            },
-            {
-              path: "course",
-              element: <CourseTable />,
-            },
-            {
-              path: "course/create",
-              element: <AddCourse />,
-            },
-            {
-              path: "course/:courseId",
-              element: <EditCourse />,
-            },
-            {
-              path: "course/:courseId/lecture",
-              element: <CreateLecture />,
-            },
-            {
-              path: "course/:courseId/lecture/:lectureId",
-              element: <EditLecture />,
-            },
-          ],
-        },
-      ],
-    },
-  ]);
-
   return (
-    <main>
-      <ThemeProvider>
-        <RouterProvider router={appRouter} />
-      </ThemeProvider>
-    </main>
+    <ThemeProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            {/* Public / Home */}
+            <Route
+              index
+              element={
+                <>
+                  <HeroSection />
+                  <Courses />
+                </>
+              }
+            />
+            {/* Login */}
+            <Route
+              path="login"
+              element={
+                <AuthenticatedUser>
+                  <Login />
+                </AuthenticatedUser>
+              }
+            />
+            {/* Student routes */}
+            <Route
+              path="my-learning"
+              element={
+                <ProtectedRoute>
+                  <MyLearning />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="profile"
+              element={
+                <ProtectedRoute>
+                  <Profile />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="course/search"
+              element={
+                <ProtectedRoute>
+                  <SearchPage />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="course-detail/:courseId"
+              element={
+                <ProtectedRoute>
+                  <CourseDetail />
+                </ProtectedRoute>
+              }
+            />
+            <Route
+              path="course-progress/:courseId"
+              element={
+                <ProtectedRoute>
+                  <PurchaseCourseProtectedRoute>
+                    <CourseProgress />
+                  </PurchaseCourseProtectedRoute>
+                </ProtectedRoute>
+              }
+            />
+
+            {/* Admin routes */}
+            <Route
+              path="admin"
+              element={
+                <AdminRoute>
+                  <Sidebar />
+                </AdminRoute>
+              }
+            >
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="course" element={<CourseTable />} />
+              <Route path="course/create" element={<AddCourse />} />
+              <Route path="course/:courseId" element={<EditCourse />} />
+              <Route
+                path="course/:courseId/lecture"
+                element={<CreateLecture />}
+              />
+              <Route
+                path="course/:courseId/lecture/:lectureId"
+                element={<EditLecture />}
+              />
+            </Route>
+          </Route>
+        </Routes>
+      </HashRouter>
+    </ThemeProvider>
   );
 }
 
