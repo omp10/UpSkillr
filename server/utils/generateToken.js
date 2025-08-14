73ae4e9970ad06ab5dbcc9593f4f1f531f3dc3af
omp10 <omparteki2002@gmail.com>
2025-08-14 22:13:14 +0530

    test 3.5

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -9,11 +9,13 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "none",
+      secure: true,        // 🔹 Required for HTTPS (Render)
+      sameSite: "none",    // 🔹 Allows cross-site cookies
       maxAge: 24 * 60 * 60 * 1000, // 1 day
-    }).json({
-        success:true,
-        message,
-        user
+    })
+    .json({
+      success: true,
+      message,
+      user,
     });
 };
